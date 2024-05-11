c2c0b12c3eca11e898de8b515aa90385e801ccf7
NikolaBundic <nikolabundic@hotmail.com>
2024-05-11 02:25:19 -0700

    Added GetAllCinemas query.

---
+++ Code9.API/Controllers/CinemaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Code9.Domain.Models;
+using Code9.Domain.Queries;
+
+namespace Code9.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CinemaController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CinemaController(IMediator mediator) {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Cinema>>> GetAllCinema() {
+            var query = new GetAllCinemaQuery();
+            var cinema = await _mediator.Send(query);
+            return Ok(cinema);
+        }
+    }
+}
